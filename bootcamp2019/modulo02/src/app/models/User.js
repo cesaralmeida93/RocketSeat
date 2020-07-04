f3f1ca65169c8a6fc47b1474cac6baa244bff3c2
cesar <ocesaralmeida@gmail.com>
2020-07-04 20:53:44 -0300

    finished JWT auth

---
+++ bootcamp2019/modulo02/src/app/models/User.js
@@ -0,0 +1,30 @@
+import { Model, Sequelize } from 'sequelize';
+import bcrypt, { hash } from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      user.password_hash = await bcrypt.hash(user.password_hash, 8);
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
